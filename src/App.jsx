e14e52c371d34f8c38ee7b8741e14a3e334071f5
Xinia <neko_1996@mail.ru>
2024-01-17 14:26:18 -0600

    Create geoButton
    Created design of geoButton and started to work on functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SearchString from "./Components/Search string";
 import st from "./styles.module.css";
 import "./reset.css";
+import GeoButton from "./Components/Geo button"
 
 function App() {
   return (
     <div className={st.widget}>
       <SearchString />
+      <GeoButton />
     </div>
   );
 }
